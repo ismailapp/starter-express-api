78baee21b55a4fe1af38119f7dd96aded67b6c8b
ISMAIL SAMUDRA <119024413+ismailapp@users.noreply.github.com>
2023-02-15 00:12:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post("/dev", jsonParser, [
                  akses:'dev',
                  nama:'ISMAIL SAMUDRA',
                  email:'ubaidfawaz18@gmail.com',
-                 hp:'083136245050,
+                 hp:'083136245050',
                  img
             }
           });
